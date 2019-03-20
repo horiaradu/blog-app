b545bfca07fcb41f7553a0b810a0623fb3035abd
David Porosnicu <david94_p@yahoo.com>
2019-03-20 11:00:23 +0200

    add tests for tags array for Post comp

--- src/tests/components/Post.test.js
+++ src/tests/components/Post.test.js
@@ -10,9 +10,26 @@ describe('Post Component', () => {
   test('should render Post component correctly', () => {
     const post = {
       comments: [],
-      entry: {}
+      entry: {
+        title: 'title',
+        body: 'body text',
+        tags: ['tag1', 'tag2']
+      }
     };
     const wrapper = shallow(<Post post={post} />);
     expect(toJSON(wrapper)).toMatchSnapshot();
+    expect(wrapper.find('li').length).toBeGreaterThan(0);
+  });
+  test('should not render tags if no tags provided', () => {
+    const post = {
+      comments: [],
+      entry: {
+        title: 'title',
+        body: 'body text',
+        tags: []
+      }
+    };
+    const wrapper = shallow(<Post post={post} />);
+    expect(wrapper.find('li').length).toBe(0);
   });
 });
